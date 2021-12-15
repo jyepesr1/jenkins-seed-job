98cb25664634f924fa6ae2039474cf8588006a21
Johan Yepes <johanyepes@hotmail.com>
2021-12-15 13:22:45 -0500

    Adding jobs definitions

--- jobs/teamA/job.groovy
+++ jobs/teamA/job.groovy
@@ -1,5 +1,9 @@
-import parent.util.Job
-
+def PATH_TO_SCRIPTS = new File(getClass().protectionDomain.codeSource.location.path).parent
+ 
+File jobClassFile = new File("${PATH_TO_SCRIPTS}/util", "job.groovy");
+Class jobClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(jobClassFile);
+GroovyObject Job = (GroovyObject) jobClass.newInstance();
+ 
 folder('teamA') {
   description('Folder containing all jobs for teamA')
 }
