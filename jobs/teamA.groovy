fa3ba0395036bb028af78c1a15295005e8c8e4c1
Johan Yepes <johanyepes@hotmail.com>
2021-12-15 13:37:43 -0500

    Adding jobs definitions

---
+++ jobs/teamA.groovy
@@ -0,0 +1,7 @@
+import util.Job
+
+folder('teamA') {
+  description('Folder containing all jobs for teamA')
+}
+
+Job.createMultibranchJob("HelloWorld", "teamA", "jyepesr1", "jenkins-test", null, 10, 10)
