fe1383b7de59bbab54daa6cd352e0e2218eededf
Johan Yepes <johanyepes@hotmail.com>
2021-12-17 14:14:00 -0500

    Reading yaml file for config jobs

---
+++ config.groovy
@@ -0,0 +1,15 @@
+import util.JobFactory
+import org.yaml.snakeyaml.Yaml
+
+def workDir = SEED_JOB.getWorkspace()
+def config = new Yaml().load(("${workDir}/config.yml" as File).text)
+
+for (team in config.teams) {
+  JobFactory factory = new JobFactory(this, team.name, team.repoOrg, team.scmCredentialsID)
+
+  factory.createTeamFolder()
+
+  for (project in team.projects) {
+    factory.createMultibranchPipeline(project.name, project.repo, project.numJobsToKeep, project.daysToKeepJobs)
+  }
+}
