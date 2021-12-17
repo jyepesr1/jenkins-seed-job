69a0387723bafdb075536966ed30896cd917bf31
Johan Yepes <johanyepes@hotmail.com>
2021-12-17 13:31:43 -0500

    Adding pipeline-utility-steps in Jenkins

--- jobs/teamA.groovy
+++ jobs/teamA.groovy
@@ -1,5 +1,14 @@
 import util.JobFactory
 
+def datas = readYaml text: """
+something: 'my datas'
+size: 3
+isEmpty: false
+"""
+assert datas.something == 'my datas'
+assert datas.size == 3
+assert datas.isEmpty == false
+
 JobFactory factory = new JobFactory(this, "teamA", "jyepesr1", "")
 factory.createTeamFolder()
 
